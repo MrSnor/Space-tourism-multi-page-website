6dadac4f82e184d3ff1c78e29be3bb794fa54e4f
Partha Priyadarshi Das <parthap.das36@gmail.com>
2022-04-08 22:37:18 +0530

    feat: :sparkles: add in-page tabs to destination page
    used data from json file instead of adding tabs to html

--- index.js
+++ index.js
@@ -18,9 +18,66 @@ navToggle.addEventListener("click", () => {
   }
 })
 
-// TODO add this later; to close menu when it loses focus
+// to close menu when it loses focus
 navToggle.addEventListener('blur', () => {
   console.log("blurred")
   nav.setAttribute("data-visible", "false")
   navToggle.setAttribute("aria-expanded", "false")
 })
+
+// ~~ for in-page tabs ~~
+
+let tabsJSONData;
+
+fetch('./data.json')
+  .then(response => response.json())
+  .then(data => {
+    tabsJSONData = data;
+  })
+
+const destTabs = document.querySelectorAll("#destination-tabs > *")
+
+// in-page tab manipulation for destination page
+destTabs.forEach((value, index) => {
+
+  // listen for click on tabs
+  value.addEventListener('click', (e) => {
+    
+    removeBorder()
+    // add border to selected tab
+    e.target.setAttribute("aria-selected", "true")
+
+    // get id of parent div to be used to relate data from json file
+    const reqTabId = e.target.parentElement.id
+    // get respective data of clicked tab by relating it to index
+    const respectiveDataFromJSON = tabsJSONData[reqTabId][index]
+
+    const planetName = document.querySelector("#planetName")
+    const planetDescription = document.querySelector("#description")
+    const planetImages = document.querySelector("#main > picture")
+    const planetDistance = document.querySelector("#distance")
+    const planetTravel = document.querySelector("#travel")
+
+    // replacing html values with data from json file
+
+    planetName.textContent = respectiveDataFromJSON.name
+
+    planetDescription.textContent = respectiveDataFromJSON.description
+
+    planetDistance.textContent = respectiveDataFromJSON.distance
+
+    planetTravel.textContent = respectiveDataFromJSON.travel
+
+    planetImages.children[0].srcset = respectiveDataFromJSON.images.webp
+
+    planetImages.children[1].src = respectiveDataFromJSON.images.png
+
+
+    e.preventDefault()
+  })
+});
+
+// to remove "highlight/selected border" from tab 
+function removeBorder() {
+  destTabs.forEach(item => item.setAttribute("aria-selected", "false"))
+}
